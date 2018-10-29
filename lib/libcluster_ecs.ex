262b7949355bcc62f5927d109295e567d0cbeda9
Daniel Paul Searles <daniel.paul.searles@gmail.com>
2018-10-28 22:29:04 -0700

    Init: mix new libcluster_ecs --module LibclusterECS

---
+++ lib/libcluster_ecs.ex
@@ -0,0 +1,18 @@
+defmodule LibclusterECS do
+  @moduledoc """
+  Documentation for LibclusterECS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LibclusterECS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/libcluster_ecs_test.exs
@@ -0,0 +1,8 @@
+defmodule LibclusterECSTest do
+  use ExUnit.Case
+  doctest LibclusterECS
+
+  test "greets the world" do
+    assert LibclusterECS.hello() == :world
+  end
+end
